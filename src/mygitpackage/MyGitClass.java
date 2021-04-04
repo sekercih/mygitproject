bba792b502eb2325243a3dce4499ab7a41eba1a9
sekercih <67022880+sekercih@users.noreply.github.com>
2021-04-04 19:07:56 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,7 +5,8 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My secon commit");
-		
+		System.out.println("buraya yenikod yazildi");
+		System.out.println("degisiklik yapildi");
 		System.out.println("My third commit");
 		System.out.println("My 4th commit");
 		System.out.println("buraya yenikod yazildi");
