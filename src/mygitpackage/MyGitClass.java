68ff197f437b5c04c511e217c446d75389b1f2fc
sekercih <67022880+sekercih@users.noreply.github.com>
2021-04-04 18:13:04 +0300

    Update MyGitClass.java
    Githup.com yeni version.

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,13 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My secon commit");
+		
+		System.out.println("My third commit");
+		System.out.println("My 4th commit");
+		System.out.println("My 5 commit");
+		
+		System.out.println("uzk sunucda ki kodlarim");
+		
 
 	}
 
