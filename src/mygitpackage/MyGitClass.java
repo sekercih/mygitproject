a287f40e6739c0d63e2a2bd9cb2b7d9cde07bcd4
sekercih <hkkskrc@gmail.com>
2021-04-04 19:24:44 +0300

    local

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,8 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My secon commit");
-		
-		System.out.println("My third commit");
+				System.out.println("My third commit");
 		System.out.println("My 4th commit");
 		System.out.println("buraya yenikod yazildi");
 		System.out.println("degisiklik yapildi");
