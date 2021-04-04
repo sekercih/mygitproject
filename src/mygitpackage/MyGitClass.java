60cf29829fa9b39de53ffd24ffd3bdb68a5db630
sekercih <67022880+sekercih@users.noreply.github.com>
2021-04-04 19:29:56 +0300

    Update MyGitClass.java
    locadan son

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,20 +5,13 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My secon commit");
-
-				System.out.println("My third commit");
-
-		
+                System.out.println("My third commit");
 		System.out.println("buraya yenikod yazildi");
 		System.out.println("degisiklik yapildi");
-		
 		System.out.println("My third commit");
-
 		System.out.println("My 4th commit");
-		
 		System.out.println("buraya yenikod yazildi");
 		System.out.println("degisiklik yapildi");
-		
 		System.out.println("buraya yenikod yazildi");
 		System.out.println("buraya yenikod yazildi");
 		
